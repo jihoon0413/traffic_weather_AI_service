b9ad68dcd2d6b9c2ea44c6eb7fa1cf7fa740c438
jihoon <wlgnstls0413@naver.com>
2025-11-26 22:48:31 +0900

    feat: 프론트 - 서버- 외부api연결
    외부 api의 데이터를 조회해 프론트에 전달(cors 문제 해결)

---
+++ ai_model/src/main/java/com/capstone/ai_model/service/GwangjuBusService.java
@@ -0,0 +1,35 @@
+package com.capstone.ai_model.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class GwangjuBusService {
+
+    private final WebClient webClient;
+
+    @Value("${open-api.secret-key}")
+    private String secretKey;
+
+    private static final String BASE_URL = "http://api.gwangju.go.kr/xml/stationInfo";
+
+
+    public Mono<String> getRouteList(String ars_id) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("http")
+                        .host("api.gwangju.go.kr")
+                        .path("/json/lineStationInfo")
+                        .queryParam("serviceKey", secretKey)
+                        .queryParam("LINE_ID", ars_id)
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+}
